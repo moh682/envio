f307c04a1e3108c06dbf038ea480fee808deae5e
Mohammad Hariri <moh.050196@gmail.com>
2025-03-28 01:49:16 +0100

    refactor(invoice): relocate http logic in their own folder

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -2,15 +2,15 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 	"strconv"
 
 	"github.com/moh682/envio/backend/internal/api/middlewares"
 	"github.com/moh682/envio/backend/internal/domain/invoice"
+	invoice_http "github.com/moh682/envio/backend/internal/domain/invoice/http"
 	invoice_repositories "github.com/moh682/envio/backend/internal/domain/invoice/repositories"
 )
-
-// Server is the API server
 
 type Server interface {
 	ListenAndServe(port int) error
@@ -24,8 +24,9 @@ func NewHttpServer(db *sql.DB) Server {
 
 	invoiceRepository := invoice_repositories.NewPostgres(db)
 	invoiceService := invoice.NewService(invoiceRepository)
-	invoiceController := invoice.NewHttpController(invoiceService)
+	invoiceController := invoice_http.NewHttpController(invoiceService)
 
+	// TODO: extract invoice routes into their own function
 	invoiceRoutes := http.NewServeMux()
 	invoiceRoutes.HandleFunc("GET /invoices", invoiceController.ListInvoices())
 	invoiceRoutes.HandleFunc("POST /invoices", invoiceController.CreateInvoice())
@@ -36,6 +37,11 @@ func NewHttpServer(db *sql.DB) Server {
 }
 
 func (s *httpServer) ListenAndServe(port int) error {
+
+	if port == 0 {
+		return errors.New("port argument cannot be 0")
+	}
+
 	addr := ":" + strconv.Itoa(port)
 	server := middlewares.Combine(middlewares.Logger(s.mux))
 	return http.ListenAndServe(addr, server)
